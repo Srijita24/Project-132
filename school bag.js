49bccd8f00a4d86fb350fffd4a99f2f2040cecee
Srijita24 <83343246+Srijita24@users.noreply.github.com>
2022-04-04 10:54:41 +0530

    Add files via upload

---
+++ school bag.js
@@ -0,0 +1,32 @@
+status = "";
+
+function preload() {
+    img = loadImage('school bag.jpg');
+}
+
+function  setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+    status = true;
+    objectDetector.detect(img, gotresult);
+}
+
+function gotresult(error, results) {
+    if (error)
+    {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
